327d022387dcc5e3ea15bbccde4820b5616d42c4
mafioz359 <mishazlupko@gmail.com>
2023-06-28 17:54:30 +0300

    hw-2

--- App.js
+++ App.js
@@ -1,20 +1,145 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+import { useState } from "react";
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { LoginScreen } from "./Screens/LoginScreen";
 
-export default function App() {
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Image,
+} from "react-native";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
+const MainStack = createStackNavigator();
+
+export default App = () => {
+  const [state, setstate] = useState(initialState);
+  const [isShowBtn, setIsShowBtn] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-regular": require("./fonts/Roboto-Regular.ttf"),
+    "Roboto-medium": require("./fonts/Roboto-Medium.ttf"),
+    "Roboto-bold": require("./fonts/Roboto-Bold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const kayBoardHide = () => {
+    setIsShowBtn(true);
+    Keyboard.dismiss();
+    console.log(state);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <MainStack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <MainStack.Screen
+            name="Registration"
+            component={RegistrationScreen}
+          />
+          <MainStack.Screen name="Login" component={LoginScreen} />
+        </MainStack.Navigator>
+      </View>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
+    ...Platform.select({
+      ios: { justifyContent: "center" },
+      android: { justifyContent: "flex-end" },
+    }),
+  },
+
+  textReg: {
+    color: "#212121",
+    fontFamily: "Roboto-medium",
+    fontSize: 30,
+    lineHeight: 35,
+    marginLeft: 100,
+  },
+  textInp: {
+    borderWidth: 1,
+    borderColor: "#BDBDBD",
+    borderRadius: 6,
+    color: "#212121",
+    backgroundColor: "#E8E8E8",
+    paddingLeft: 15,
+    paddingTop: 14,
+    paddingBottom: 14,
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    textAlign: "left",
+  },
+  form: {
+    marginHorizontal: 16,
+    paddingBottom: 82,
+    justifyContent: "center",
+    marginTop: 90,
+  },
+  inputContainer: {
+    marginTop: 16,
+  },
+  btnReg: {
+    backgroundColor: "#FF6C00",
+    height: 51,
+    borderRadius: 100,
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  textBtn: {
+    color: "#fff",
+    fontFamily: "Roboto-regular",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    textAlign: "center",
+  },
+  bgForm: {
+    backgroundColor: "#fff",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    // bottom: -60,
+  },
+  mgFoto: {
+    bottom: 60,
+    marginLeft: 110,
+    position: "absolute",
+  },
+  addIcon: {
+    position: "relative",
+    top: -80,
+    left: 215,
   },
 });
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,216 @@
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+import { useEffect, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Image,
+} from "react-native";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
+export const RegistrationScreen = () => {
+  const [state, setstate] = useState(initialState);
+  const [isShowBtn, setIsShowBtn] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-regular": require("../fonts/Roboto-Regular.ttf"),
+    "Roboto-medium": require("../fonts/Roboto-Medium.ttf"),
+    "Roboto-bold": require("../fonts/Roboto-Bold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // useEffect(() => {}, []);
+
+  const kayBoardHide = () => {
+    setIsShowBtn(true);
+    Keyboard.dismiss();
+    console.log(state);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={kayBoardHide}>
+        <ImageBackground
+          style={styles.imageBg}
+          source={require("../Screens/images/photo-bg.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.keyboardAvoidingContainer}
+          >
+            <View
+              style={{
+                ...styles.bgForm,
+                bottom: isShowBtn ? 0 : 120,
+                paddingBottom: isShowBtn ? 0 : 0,
+              }}
+            >
+              <View style={styles.form}>
+                <View>
+                  <Image
+                    source={require("../Screens/images/Rectangle-22.png")}
+                    style={styles.mgFoto}
+                  />
+                  <AntDesign
+                    name="pluscircleo"
+                    size={24}
+                    color="#FF6C00"
+                    style={styles.addIcon}
+                  />
+                </View>
+                <Text style={styles.textReg}>Реєстрація</Text>
+                <View style={styles.inputContainer}>
+                  <TextInput
+                    style={styles.textInp}
+                    placeholder="Логін"
+                    onFocus={() => setIsShowBtn(false)}
+                    onChangeText={(value) =>
+                      setstate((prevState) => ({ ...prevState, login: value }))
+                    }
+                  />
+                </View>
+                <View style={styles.inputContainer}>
+                  <TextInput
+                    style={styles.textInp}
+                    placeholder="Адреса електронної пошти"
+                    onFocus={() => setIsShowBtn(false)}
+                    onChangeText={(value) =>
+                      setstate((prevState) => ({ ...prevState, email: value }))
+                    }
+                  />
+                </View>
+                <View style={styles.inputContainer}>
+                  <TextInput
+                    style={styles.textInp}
+                    placeholder="Пароль"
+                    secureTextEntry={true}
+                    onFocus={() => setIsShowBtn(false)}
+                    onChangeText={(value) =>
+                      setstate((prevState) => ({
+                        ...prevState,
+                        password: value,
+                      }))
+                    }
+                  />
+                </View>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  style={styles.btnReg}
+                  onPress={kayBoardHide}
+                >
+                  <Text style={styles.textBtn}>Зареєстуватися</Text>
+                </TouchableOpacity>
+                <Text
+                  onPress={() => navigation.navigate("Login")}
+                  style={{
+                    color: "#1B4371",
+                    fontFamily: "Roboto-Regular",
+                    fontSize: 16,
+                    marginBottom: 45,
+                    marginTop: 16,
+                    marginLeft: 50,
+                  }}
+                >
+                  Вже є акаунт? Увійти
+                </Text>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
+    ...Platform.select({
+      ios: { justifyContent: "center" },
+      android: { justifyContent: "flex-end" },
+    }),
+  },
+
+  textReg: {
+    color: "#212121",
+    fontFamily: "Roboto-medium",
+    fontSize: 30,
+    lineHeight: 35,
+    marginLeft: 100,
+  },
+  textInp: {
+    borderWidth: 1,
+    borderColor: "#BDBDBD",
+    borderRadius: 6,
+    color: "#212121",
+    backgroundColor: "#E8E8E8",
+    paddingLeft: 15,
+    paddingTop: 14,
+    paddingBottom: 14,
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    textAlign: "left",
+  },
+  form: {
+    marginHorizontal: 16,
+    paddingTop: 92,
+    paddingBottom: 82,
+  },
+  inputContainer: {
+    marginTop: 16,
+  },
+  btnReg: {
+    backgroundColor: "#FF6C00",
+    height: 51,
+    borderRadius: 100,
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  textBtn: {
+    color: "#fff",
+    fontFamily: "Roboto-regular",
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 19,
+    textAlign: "center",
+  },
+  bgForm: {
+    backgroundColor: "#fff",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    bottom: -60,
+  },
+  mgFoto: {
+    bottom: 60,
+    marginLeft: 110,
+    position: "absolute",
+  },
+  addIcon: {
+    position: "relative",
+    top: -80,
+    left: 215,
+  },
+});
